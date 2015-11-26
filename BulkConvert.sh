2b9faf1dff50dac0121dbad78be3de0f15e22f04
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2015-11-26 12:24:53 -0600

    Update BulkConvert.sh

--- BulkConvert.sh
+++ BulkConvert.sh
@@ -2,14 +2,16 @@
 #Using Calibre Command line utilities for the command ebook-convert
 #Raw files (epub or pdf) are distributed into the respective folders named: 'epub' and 'pdf'
 
-for book in /Users/srayanguhathakurta/Dropbox/Apps/novels/epub/*
+epubPATH=/path/to/epub/*
+pdfPATH=/path/to/pdf/*
+for book in $epubPATH
 do
         echo "Converting $book";
         ebook-convert "$book" "$(basename "$book" .epub).mobi";
 done
 
 
-for book in /Users/srayanguhathakurta/Dropbox/Apps/novels/pdf/*
+for book in $pdfPATH
 do
         echo "Converting $book";
         ebook-convert "$book" "$(basename "$book" .pdf).mobi";
